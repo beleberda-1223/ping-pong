b653e00bd595808c95bf3fcbc157418630789578
beleberda-1223 <beleberdapon@gmail.com>
2026-01-22 18:56:56 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,6 +1,7 @@
 from pygame import *
 
 green = (102, 255, 102)
+red = (255, 0, 0)
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
@@ -22,19 +23,19 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update_r(self):
-        keys = key.get_pressed()
-        if keys[K_w] and self.rect.y < 400:
-            self.rect.y += self.speed
-        if keys[K_s] and self.rect.y > 100:
-            self.rect.y += self.speed
-
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y < 400:
+        if keys[K_s] and self.rect.y < 400:
             self.rect.y += self.speed
-        if keys[K_DOWN] and self.rect.y > 100:
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
 
 clock = time.Clock()
 FPS = 60
@@ -46,18 +47,46 @@ ball = GameSprite('ball.png', 350, 250, 50, 50, 5)
 run = True
 finish = False
 
+font.init()
+font = font.Font(None, 35)
+lose_1 = font.render('Player 1 lose!', True, red)
+lose_2 = font.render('Player 2 lose!', True, red)
+
+s_x = 3
+s_y = 3
+
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     
+    if not finish:
 
-    player_1.update_r()
-    player_1.reset()
-    player_2.update_l()
-    player_2.reset()
-    ball.update()
-    ball.reset()
+        window.fill(green)
+        player_1.update_r()
+        player_1.reset()
+        player_2.update_l()
+        player_2.reset()
+        ball.update()
+        ball.reset()
+        ball.rect.x += s_x
+        ball.rect.y += s_y
+
+        if sprite.collide_rect(ball, player_1) or sprite.collide_rect(ball, player_2):
+            s_x *= -1
+            s_y *= 1
+
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            s_y *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose_1, (270, 210))
+
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose_2, (270, 210))
+            
 
 
     display.update()
